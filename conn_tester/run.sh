faa2195186ce4089fdf85adedb3f1ab0205b298f
Miguel A. Munoz <magonzalez@fortinet.com>
2021-07-14 10:46:39 +0200

    Add WebFilter testing capabilities

--- conn_tester/run.sh
+++ conn_tester/run.sh
@@ -8,6 +8,9 @@ NC="\e[0m"
 wget_options='-qO- -T 30 -t 1'
 curl_options='-s -o /dev/null'
 
+#=============================
+# Initial connectivity
+#=============================
 echo
 echo -e "${YEL}General connectivity tests${NC}"
 echo "=========================="
@@ -22,30 +25,36 @@ else
 fi
 echo "(1/1)."
 
+#=============================
+# EICAR (AV/IPS)
+#=============================
 echo
 echo "Downloading EICAR (3):"
 wget ${wget_options} http://www.rexswain.com/eicar.com > /dev/null
 if [ $? -ne 0 ]; then
     echo -ne "${GRE}Ok:${NC} EICAR cannot be downloaded"
 else
-    echo -ne "${RED}Error:${ENDCOLOR} EICAR can be downloaded"
+    echo -ne "${RED}Error:${NC} EICAR can be downloaded"
 fi
 echo "(1/3)."
 wget ${wget_options} http://www.rexswain.com/eicar.zip > /dev/null
 if [ $? -ne 0 ]; then
     echo -ne "${GRE}Ok:${NC} EICAR cannot be downloaded"
 else
-    echo -ne "${RED}Error:${ENDCOLOR} EICAR can be downloaded"
+    echo -ne "${RED}Error:${NC} EICAR can be downloaded"
 fi
 echo "(2/3)."
 wget ${wget_options} http://www.rexswain.com/eicar2.zip > /dev/null
 if [ $? -ne 0 ]; then
     echo -ne "${GRE}Ok:${NC} EICAR cannot be downloaded"
 else
-    echo -ne "${RED}Error:${ENDCOLOR} EICAR can be downloaded"
+    echo -ne "${RED}Error:${NC} EICAR can be downloaded"
 fi
 echo "(3/3)."
 
+#=============================
+# Files (MP3)
+#=============================
 echo
 echo "Downloading MP3 files (2):"
 timeout 30 wget ${wget_options} http://www.gurbaniupdesh.org/multimedia/01-Audio%20Books/Baba%20Noadh%20Singh/000%20Introduction%20Bhai%20Sarabjit%20Singh%20Ji%20Gobindpuri.mp3  > /dev/null
@@ -64,6 +73,9 @@ else
 fi
 echo "(2/2)."
 
+#=============================
+# DLP
+#=============================
 echo
 echo "Checking DLP(4)"
 echo "  Credit Card (Amex):"
@@ -102,23 +114,44 @@ else
 fi
 echo "(4/4)."
 
+#=============================
+# Viruses
+#=============================
 echo
 echo "Downloading Viruses (2):"
 wget ${wget_options} http://www.esthetique-realm.net/ > /dev/null
 if [ $? -ne 0 ]; then
     echo -ne "${GRE}Ok:${NC} JS/Iframe.BYO!tr cannot be downloaded"
 else
-    echo -ne "${RED}Error:${ENDCOLOR} JS/Iframe.BYO!tr can be downloaded"
+    echo -ne "${RED}Error:${NC} JS/Iframe.BYO!tr can be downloaded"
 fi
 echo "(1/2)."
 wget ${wget_options} http://www.newalliancebank.com/ > /dev/null
 if [ $? -ne 0 ]; then
     echo -ne "${GRE}Ok:${NC} HTML/Refresh.250C!tr cannot be downloaded"
 else
-    echo -ne "${RED}Error:${ENDCOLOR} HTML/Refresh.250C!tr can be downloaded"
+    echo -ne "${RED}Error:${NC} HTML/Refresh.250C!tr can be downloaded"
 fi
 echo "(2/2)."
 
+#=============================
+# WebFilter
+#=============================
+sites=(www.magikmobile.com www.cstress.net www.ilovemynanny.com ww1.movie2kproxy.com www.microsofl.bid)
+echo
+echo "Checking WebFilter (${#sites[@]}):"
+i=0
+for site in ${sites[@]}
+do
+    i=$(($i+1))
+    wget ${wget_options} ${site} > /dev/null
+    if [ $? -ne 0 ]; then
+        echo -ne "${GRE}Ok:${NC} ${site} cannot be accessed"
+    else
+        echo -ne "${RED}Error:${NC} ${site} can be accessed"
+    fi
+    echo "(${i}/${#sites[@]})."
+done
 
 
 # wget --no-check-certificate https://secure.eicar.org/eicar.com
