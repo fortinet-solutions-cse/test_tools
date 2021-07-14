d0f1ba80706b0a4b21eafd7aeae7e2ec38c14a0c
Miguel A. Munoz <magonzalez@fortinet.com>
2021-07-14 12:04:56 +0200

    Add more sitest to AppControl

--- conn_tester/run.sh
+++ conn_tester/run.sh
@@ -157,7 +157,7 @@ done
 #=============================
 # AppControl
 #=============================
-sites=(www.logmeinrescue.com)
+sites=(www.logmeinrescue.com unblockvideos.com)
 echo
 echo "Checking AppControl (${#sites[@]}):"
 i=0
